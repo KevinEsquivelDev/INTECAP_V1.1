792391a657680d9b49cb53f6d0c4d4b926378dc2
Kevin Esquivel <kevinesquiveldev@gmail.com>
2022-09-18 15:20:52 -0600

    Login Actualizado

---
+++ public/js/loginSs.js
@@ -0,0 +1,7 @@
+Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Inicio de sesión exitoso',
+    showConfirmButton: false,
+    timer: 1500
+})
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
     <div id="app">
@@ -67,6 +68,7 @@
                                     </form>
                                 </div>
                             </li>
+                            <script src="{!! asset('js/loginSs.js') !!}"></script>
                         @endguest
                     </ul>
                 </div>
